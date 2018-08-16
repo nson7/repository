2f7a213a9b5500e5ba0969915928f2c749fd9446
nson7 <nson7@sina.com>
2018-08-16 13:50:40 +0800

    add gcc.sh 2018-08-16

---
+++ gcc.sh
@@ -0,0 +1,7 @@
+gcc aviocat.c -lavutil -lavformat -lavdevice -lswscale -lavcodec -lswresample -lavfilter -lSDL2 -lSDL2main
+# gcc aviocat.c -lavutil -lavformat -lavdevice -lswscale -lavcodec -lswresample -lavfilter -lSDL2 -lSDL2main
+# gcc main.c -o test -I/usr/local/ffmpeg/include -L/usr/local/ffmpeg/lib -lavformat -lavcodec -lavutil -lswscale -lavfilter
+# gcc main.c -o test -I/usr/local/ffmpeg/include -L/usr/local/ffmpeg/lib -lavcodec
+# gcc main.c -o test -lavcodec
+# gcc aviocat.c -lavutil -lavformat -lavdevice -lswscale -lavcodec -lswresample -lavfilter -lSDL2 -lSDL2main
+
