80d2596bcb914addc0b02b76e295450dc9422722
nson7 <nson7@sina.com>
2018-11-19 14:18:44 +0800

    Greasemonkey.zip 2018-11-19

---
+++ projects.sh
@@ -0,0 +1,2 @@
+java -jar /media/nson7/source/nson7/opengrok-1.1-rc23/lib/opengrok.jar -P -S -v -s /media/nson7/source/nson7/opengrok-1.1-rc23/projects -d /media/nson7/source/nson7/opengrok-1.1-rc23/data -W /media/nson7/source/nson7/opengrok-1.1-rc23/data/configuration.xml
+
